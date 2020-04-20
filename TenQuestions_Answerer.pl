57c3255cfc728b737187d8e159af5749a7771a23
djtfoo <djt.foo@gmail.com>
2020-04-20 20:09:19 +0800

    Added facts by attribute, so the list of options can be printed more collectively and more unique responses can be crafted to the user's questions

--- TenQuestions_Answerer.pl
+++ TenQuestions_Answerer.pl
@@ -33,7 +33,7 @@ start_game() :-
     read(Option),
     retractall(first_time()),
     % initialize game - choose a Pokemon
-    init_game(10),
+    init_game(),
 
     % print options for user
     write('\nAlright! Since there\'s so many Pokemon, I\'ll limit it to just a few. Here\'s a list of attributes among the possible Pokemon options.\n\n'),
@@ -50,7 +50,7 @@ game_loop(C) :-
     % if user has asked 10 questions already
     C == 0 ->
     (
-        write('\nAlright, you\'ve used up all your questions! Try making a guess now!\n(Guess the Pokemon) : '),
+        write('\nAlright, you\'ve used up all your questions! Try making a guess now!\n(Guess the Pokemon) '),
         read(Guess),
         check_guess(Guess),
         write('\n\n'),
@@ -62,43 +62,29 @@ game_loop(C) :-
         write('Questions Left: '),
         write(C),
         % get option from user
-        write('\nIs it ... : '),
+        write('\nIs it ... '),
         read(X),
-        % first, check if user made a correct guess
         (
-            selected(X) -> write('Correct! Great job!\n\n'), start();
-            % get list of attributes of the selected Pokemon
+            % first, check if user made a correct guess
+            selected(X) -> write('Correct! Great job!\n\n'),
+            % start a new round
+            start()
+         ;
+            % else, get list of attributes of the selected Pokemon
             selected(Y), get_attributes(Y, L),
             % check if X is an attribute of the selected Pokemon
             (
-               member(X, L) -> write('Yes, it is ');
-               write('No, it\'s not ')
+               member(X, L) -> Truth = true; Truth = false
             ),
-            write(X), write('.\n'),
+            % print response to user
+            print_response(X, Truth),
+            write('\n'),
 
             % decrement counter
             countdown(C, NewC),
             % go to next iteration
             game_loop(NewC)
         )
-    ).
-
-% User asks if X is true.
-has(X) :-
-    % Check counter to see if 10 questions have been used up
-    qn_counter(C),
-    % If user has asked 10 questions already
-    C == 0 ->
-    (   write('Hey, no more asking! Try making a guess now!')
-    );
-    (   % get list of attributes of the selected Pokemon
-        selected(Y), get_attributes(Y, L),
-        % check if X is an attribute of the selected Pokemon
-        (member(X, L) -> write('Yes\n'); write('No\n')),
-        % decrement counter
-        counter_decr(NewC),
-        write('Questions asked: '),
-        write(NewC)
     ).
 
 % Check the guess made by the user.
@@ -113,16 +99,13 @@ list_options() :-
     all_options(X), write(X).
 
 % Randomly selects a Pokemon and starts a new round of Ten Questions.
-init_game(NumQns) :-
+init_game() :-
     % remove existing Pokemon choice (if any)
     retractall(selected(_)),
     % randomly pick another Pokemon
     random_selection(X),
     % assert the picked Pokemon as the choice
-    assert(selected(X)),
-    % reset counter
-    counter_init(NumQns).
-    %Goal = selected(Y).
+    assert(selected(X)).
 
 % get attributes of the selected Pokemon
 get_attributes(X, L) :-
@@ -133,50 +116,71 @@ get_attributes(X, L) :-
     X = haunter -> haunter(L);
     X = goldeen -> goldeen(L);
     X = kadabra -> kadabra(L);
-    X = electrode -> electrode(L).
+    X = electrode -> electrode(L);
+    X = zubat -> zubat(L).
 
-% Global Counter (not in use)
-:- dynamic(qn_counter/1).  % allow counter to be set at runtime
-
-counter_init(C) :-
-    retractall(qn_counter(_)),  % remove current counter value (if any)
-    assert(qn_counter(C)).  % set counter value = C
-
+% Call for counting down a value
 countdown(C0, C) :-
     C is C0 - 1.
 
-counter_decr(C) :-
-    qn_counter(C0),  % get current counter value
-    retractall(qn_counter(_)),  % remove current counter value
-    countdown(C0, C),  % get next counter value
-    assert(qn_counter(C)).  % set next counter value
-
 % Facts about the possible Pokemon
-pikachu([electric, monotype, yellow, quadruped, stage_basic, evolve-by-stone, static]).
-lapras([water, monotype, blue, fish, does_not_evolve, water_absorb]).
-mewtwo([psychic, monotype, legendary, purple, upright, does_not_evolve, mega_variant]).
-charizard([fire, flying, red, upright, stage_final, mega_variant, blaze]).
-haunter([ghost, poison, purple, arms, stage_intermediate, evolve_by_trade, levitate]).
-goldeen([water, monotype, fish, red, stage_basic, evolve_by_level, swift_swim]).
-kadabra([psychic, monotype, upright, brown, stage_intermediate, evolve_by_trade, synchronize]).
-electrode([electric, monotype, ball, red, stage_final, static]).
+pikachu([electric, monotype, yellow, quadruped, basic, evolves-by-stone, static]).
+lapras([water, monotype, blue, fish, final, does-not-evolve, water-absorb]).
+mewtwo([psychic, monotype, legendary, purple, upright, final, does-not-evolve, mega-variant, pressure]).
+charizard([fire, flying, red, upright, final, does-not-evolve, mega-variant, blaze]).
+haunter([ghost, poison, purple, arms, intermediate, evolves-by-trade, levitate]).
+goldeen([water, monotype, fish, red, basic, evolves-by-level, swift-swim]).
+kadabra([psychic, monotype, upright, brown, intermediate, evolves-by-trade, synchronize]).
+electrode([electric, monotype, ball, red, final, does-not-evolve, static]).
+zubat([poison, flying, wings, purple, basic, evolves-by-level, inner-focus]).
 
 % List of possible Pokemon
-selection_list([pikachu, lapras, mewtwo, charizard, haunter, goldeen, kadabra, electrode]).
+selection_list([pikachu, lapras, mewtwo, charizard, haunter, goldeen, kadabra, electrode, zubat]).
 
-question_options([monotype, type1, type2, legendary, colour, body_shape, stage, evolve_by, ability, habitat]).
+% Possible Pokemon attributes (may or may not apply to any Pokemon)
+colour([red, yellow, blue, green, brown, purple]).
+booleans([monotype, legendary]).
+has([mega-variant]).
+type([electric, water, psychic, fire, grass, flying, ghost, poison, rock, ground]).
+shape([quadruped, fish, upright, arms, ball, wings]).
+stage([stage-basic, stage-intermediate, stage-final]).
+evolveby([does-not-evolve, evolves-by-stone, evolves-by-level, evolves-by-trade]).
+ability([static, water-absorb, pressure, blaze, levitate, swift-swim, synchronize, inner-focus]).
 
-% Possible guesses
+% List of attributes available for guessing
+all_options(X) :-  %X = the list of ALL options
+    colour(A), booleans(B), append(A, B, Temp),
+    has(C), append(Temp, C, Temp2),
+    type(D), append(Temp2, D, Temp3),
+    shape(E), append(Temp3, E, Temp4),
+    stage(F), append(Temp4, F, Temp5),
+    evolveby(G), append(Temp5, G, Temp6),
+    ability(H), append(Temp6, H, X).
+
+% Print result of question asked by user
+print_response(X, IsCorrect) :-
+    % if user asked about a colour, print this response
+    colour(L1), member(X, L1), (IsCorrect=true -> write('Yes, its colour is '); write('No, its colour is not ')),write(X), write('.');
+    % if user asked about a boolean, print this response
+    booleans(L2), member(X, L2), (IsCorrect=true -> write('Yes, it is '); write('No, it is not ')), write(X), write('.');
+    % if user asked about some trait the Pokemon has, print this response
+    has(L3), member(X, L3), (IsCorrect=true -> write('Yes, it has '); write('No, it does not have ')), write(X), write('.');
+    % if user asked about the Pokemon type, print this response
+    type(L4), member(X, L4), (IsCorrect=true -> write('Yes, it is a '); write('No, it is not a ')), write(X), write('-type.');
+    % if user asked about the body shape, print this response
+    shape(L5), member(X, L5), (IsCorrect=true -> write('Yes, its body shape is '); write('No, its body shape is not ')), write(X), write('.');
+    % if user asked about the evolutionary stage, print this response
+   stage(L6), member(X, L6), (IsCorrect=true -> write('Yes, it is in the '); write('No, it is not in the ')), write(X), write(' stage.');
+    % if user asked about the evolution methods, print this response
+    evolveby(L7), member(X, L7), (IsCorrect=true -> write('Yes, it '); write('No, it does not ')), write(X), write('.');
+    % if user asked about the Pokemon's ability, print this response
+    ability(L8), member(X, L8), (IsCorrect=true -> write('Yes, its ability is '); write('No, its ability is not ')), write(X), write('.');
+    write('Hey, ask me something only from the list!').
+
+
+% Generate a possible Pokemon selection
 random_selection(X) :-
     selection_list(L), random_member(X, L).
-
-% TO-DO: Remove duplicates in list
-all_options(X) :-  %X = the list of ALL options
-    pikachu(A), lapras(B), append(A, B, TempList),
-    mewtwo(C), append(TempList, C, TempList2),
-    charizard(D), append(TempList2, D, TempList3),
-    haunter(E), append(TempList3, E, TempList4),
-    goldeen(F), append(TempList4, F, X).
 
 % allow selected Pokemon to be set at runtime
 :- dynamic(selected/1).
